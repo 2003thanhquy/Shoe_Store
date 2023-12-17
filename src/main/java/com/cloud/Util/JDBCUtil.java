9db1b5dcdc8403267b971189bb0a15ab92b2e64d
Huynh Hong Khanh <93132254+HuynhKane@users.noreply.github.com>
2023-12-17 17:29:28 +0700

    Update UTIL file

--- src/main/java/com/cloud/Util/JDBCUtil.java
+++ src/main/java/com/cloud/Util/JDBCUtil.java
@@ -13,9 +13,9 @@ public class JDBCUtil {
         try
         {
             DriverManager.registerDriver(new com.mysql.jdbc.Driver());
-            String url = System.getenv("jdbc:mysql://34.122.67.95:3306/ShoeStore");
-            String username = System.getenv("cloud");
-            String password = System.getenv("290103");
+            String url = "jdbc:mysql://34.122.67.95:3306/ShoeStore";
+            String username = "cloud";
+            String password = "290103";
 
 
             conn=DriverManager.getConnection(url,username,password);
