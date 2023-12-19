cf8fecc6f358f0072d47f00571f26808043f0ab0
Nguyễn Trung Hậu <trunghauad02@gmail.com>
2023-12-19 21:12:05 +0700

    Update Model Invoice + getAllInvoices InvoiceDao

--- src/main/java/com/cloud/Daos/InvoiceDao.java
+++ src/main/java/com/cloud/Daos/InvoiceDao.java
@@ -1,4 +1,41 @@
 package com.cloud.Daos;
 
+import com.cloud.Models.Invoice;
+import com.cloud.Util.JDBCUtil;
+import com.cloud.Util.HandleExeption;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 public class InvoiceDao {
+
+    public List<Invoice> getAllInvoices () {
+        String sql = "SELECT * FROM Invoices";
+        List<Invoice> invoices = new ArrayList<>();
+        Connection conn = JDBCUtil.getConnection();
+
+        try {
+            PreparedStatement ps = conn.prepareStatement(sql);
+            System.out.println(ps);
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                Invoice invoice = new Invoice();
+                invoice.setInvoiceID(rs.getInt("InvoiceID"));
+                invoice.setOrderID(rs.getInt("OrderID"));
+                invoice.setInvoiceDateTime(rs.getDate("InvoiceDateTime"));
+                invoice.setTotal(rs.getDouble("Total"));
+                invoice.setStatus(rs.getInt("Status"));
+                invoice.setTypePayment(rs.getInt("TypePayment"));
+
+                invoices.add(invoice);
+            }
+        } catch (SQLException e) {
+            HandleExeption.printSQLException(e);
+        } finally {
+            JDBCUtil.closeConnection(conn);
+        }
+        return invoices;
+    }
 }
