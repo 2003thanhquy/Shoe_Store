780109e747fabc394442fd8a4d47549bc4152d49
HoangPhuc <phuchoangtran2806@gmail.com>
2023-12-20 15:56:26 +0700

    cart

--- src/main/java/com/cloud/Models/Invoice.java
+++ src/main/java/com/cloud/Models/Invoice.java
@@ -9,6 +9,9 @@ public class Invoice implements Serializable {
 
     private int invoiceID;
     private int orderID;
+
+
+
     private Date invoiceDateTime;
     private Double total;
     private int typePayment;
@@ -17,8 +20,21 @@ public class Invoice implements Serializable {
         return (typePayment == 1) ? "Credit" : "COD";
     }
 
+    public int getTypePayment() {
+        return typePayment;
+    }
+    public void setTypePayment(int typePayment) {
+        this.typePayment = typePayment;
+    }
+
     public String getStatusString() {
         return (status ==1 )? "Done": "Pending";
+    }
+    public int getStatus() {
+        return status;
+    }
+    public void setStatus(int status) {
+        this.status = status;
     }
 
     private int status;
@@ -58,19 +74,4 @@ public class Invoice implements Serializable {
         this.total = total;
     }
 
-    public int getStatus() {
-        return status;
-    }
-
-    public void setStatus(int status) {
-        this.status = status;
-    }
-
-    public int getTypePayment() {
-        return typePayment;
-    }
-
-    public void setTypePayment(int typePayment) {
-        this.typePayment = typePayment;
-    }
 }
