cd6a874acfadeb2a96c4193de42eb1e9b03a5336
2003thanhquy <quythanhhuynh2003@gmail.com>
2023-12-21 07:46:53 +0700

    model static session

---
+++ src/main/java/com/cloud/Models/Session.java
@@ -0,0 +1,9 @@
+package com.cloud.Models;
+
+public class Session {
+    public  static  String userID;
+    private static String fullName;
+    private static String email;
+    private static String role;
+    private static byte[] avatar;
+}
